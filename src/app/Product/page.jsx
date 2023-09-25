a56ebbd74ef9bee121f049f91d438c862468e33f
Aakash <aakashanand1995@gmail.com>
2023-09-25 16:18:46 +0530

    updates to navbar

---
+++ src/app/Product/page.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Product = () => {
+  return <div>Product Page</div>;
+};
+
+export default Product;
---
+++ src/app/components/Navbar/Navbar.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="w-full h-14 bg-[#f6f6eb] text-[#381fd1] font-extrabold text-sm">
+      <ul className="h-10 flex justify-center gap-8 items-center pt-2">
+        <a
+          className="flex justify-center items-center p-1 w-20 hover:border-2 rounded-md border-[#381fd1]"
+          href="/Product"
+        >
+          PRODUCT
+        </a>
+        <a
+          className="flex justify-center items-center p-1 w-20 hover:border-2 rounded-md border-[#381fd1]"
+          href="/"
+        >
+          PRICING
+        </a>
+        <a
+          className="flex justify-center items-center p-1 w-20 hover:border-2 rounded-md border-[#381fd1]"
+          href="/"
+        >
+          BLOG
+        </a>
+        <a
+          className="flex justify-center items-center p-1 w-20 hover:border-2 rounded-md border-[#381fd1]"
+          href="/"
+        >
+          LIBRARY
+        </a>
+        <a
+          className="flex justify-center items-center p-1 w-20 hover:border-2 rounded-md border-[#381fd1]"
+          href="/"
+        >
+          SUPPORT
+        </a>
+        <a
+          className="flex justify-center items-center p-1 w-20 hover:border-2 rounded-md border-[#381fd1]"
+          href="/"
+        >
+          LOG IN
+        </a>
+        <button className="bg-[#381fd1] text-white rounded-md p-1 text-sm px-4">
+          30-DAY FREE TEAM TRIAL
+        </button>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
