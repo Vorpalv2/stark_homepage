4fb313a45c3ea4fc4f81f6c3817334952bb30c5a
Aakash <aakashanand1995@gmail.com>
2023-09-26 08:56:42 +0530

    added comments

--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -4,11 +4,15 @@ import React, { useState } from "react";
 
 const Navbar = () => {
   const [style, setStyle] = useState({
+    //anchor tag styling
     aStyle:
       "flex justify-center items-center p-1 w-20 hover:border-2 rounded-md border-[#381fd1]",
+    //main nav container styling
     containerStyle:
       "w-full h-14 bg-[#f6f6eb] text-[#381fd1] font-extrabold text-sm",
+    // ui list styling
     ulStyle: "h-10 flex justify-center gap-8 items-center pt-2",
+    // nav button Styling
     buttonStyle:
       "bg-[#381fd1] hover:bg-[#7e6de9] text-white rounded-md p-1 text-sm px-4",
   });
