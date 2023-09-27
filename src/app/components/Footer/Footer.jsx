cc0e373befb5c94a8b33fb246bd655b1b4213258
Aakash <aakashanand1995@gmail.com>
2023-09-27 12:56:07 +0530

    changes

---
+++ src/app/components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footer = () => {
+  return <div className="bg-[#11284b] h-400px w-full"></div>;
+};
+
+export default Footer;
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,18 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Mainbox from "./components/Mainbox/Mainbox";
+import Footer from "./components/Footer/Footer";
 
 const Page = () => {
   return (
-    <div className="h-full w-full">
-      <Navbar />
-      <Mainbox />
+    <div>
+      <div className="h-full w-full">
+        <Navbar />
+        <Mainbox />
+      </div>
+      <div className="flex flex-col">
+        <Footer />
+      </div>
     </div>
   );
 };
